2b8b3171e14c30647abc37dda2a814c3510cea45
unknown <annop.j@IT-37NBCW1.nmt.co.th>
2014-11-15 08:14:42 +0700

    fizzbuzz

---
+++ index.php
@@ -0,0 +1,18 @@
+<? class fizzbuzz {
+ 	
+	function check_and_disp_fizzbuzz($number){
+
+		if  ($number %3 == 0){
+  			return 'FIZZ';
+		} elseif ($number %5 ==0){
+			return 'BUZZ';
+		} elseif ($number %15 ==0){
+			return 'FIZZBUZZ';
+			} else return $number;
+
+		
+	} 	
+}
+ 
+?>
+
